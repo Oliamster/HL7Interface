5b5108612f1cba67885f125a0aaabfc1dc53d392
oliamster <Oliamster@MacBook-Pro-di-Mc.local>
2019-04-27 00:15:28 +0200

    State pattern implementation for the equipment simulator

--- HL7Interface/HL7Interface/HL7Commands.cs
+++ HL7Interface/HL7Interface/HL7Commands.cs
@@ -2,13 +2,8 @@
 using HL7Interface.Configuration;
 using HL7Interface.ServerProtocol;
 using NHapiTools.Base.Util;
-using SuperSocket.ClientEngine.Protocol;
 using SuperSocket.SocketBase.Command;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HL7Interface
 {
